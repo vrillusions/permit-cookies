49b75e891fb5dbcb9b22c5b6bdc10f1d17b4aa6a
Todd Eddy <vr@vrillusions.com>
2011-10-16 23:30:28 -0400

    Add a build script

---
+++ build.bat
@@ -0,0 +1,24 @@
+@ECHO OFF
+SETLOCAL
+
+REM Build the src directory
+
+SET P7ZIP="C:\Program Files\7-Zip\7z.exe"
+
+rmdir /s /q build
+mkdir build
+xcopy /s src build
+
+REM compress pcookie.jar file
+cd build\chrome
+%P7ZIP% a pcookie.jar pcookie
+rmdir /s /q pcookie
+
+REM compress whole project
+cd ..
+REM yes this SHOULD cause an infinite loop but 7zip is better than that
+%P7ZIP% a pcookie.xpi *
+
+echo.
+echo Complete, file is in build directory
+pause
