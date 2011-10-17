b1e7da360d4f2a21acc4397d8af4da52796f9b1e
Todd Eddy <vr@vrillusions.com>
2011-10-16 23:32:01 -0400

    Make src directory more source-like.

---
+++ src/chrome/pcookie/content/pcookie/pcookie.js
@@ -0,0 +1,247 @@
+//////////////////////////////////
+// pcookie.js
+// Daniel Lindkvist
+// 2005-09-22
+//////////////////////////////////
+
+_DEBUG = false;
+
+
+var pCookieProgressListener =
+{
+  stateIsRequest:false,
+
+  QueryInterface : function(aIID)
+  {
+    if (aIID.equals(Components.interfaces.nsIWebProgressListener) ||
+        aIID.equals(Components.interfaces.nsISupportsWeakReference) ||
+        aIID.equals(Components.interfaces.nsISupports))
+        return this;
+      throw Components.results.NS_NOINTERFACE;
+  },
+
+  onStateChange : function(a,b,c,d) {},
+  onProgressChange : function(a,b,c,d,e,f){},
+  onStatusChange : function(a,b,c,d){},
+  onSecurityChange : function(a,b,c){},
+  onLinkIconAvailable : function(a){},
+
+  onLocationChange : function(aProgress, aRequest, aLocation)
+  {
+    pCookie.updateButton(aLocation);
+  }
+};
+
+
+var pCookie =
+{
+  urlbox: null,
+  permMan: null,
+
+  init: function()
+  {
+    try {
+      gBrowser.addProgressListener(pCookieProgressListener);
+    }catch(e){}
+  },
+  
+  exit: function()
+  {
+    try {
+      gBrowser.removeProgressListener(pCookieProgressListener);
+    }catch(e){}
+  },
+
+  permit: function()
+  {
+    var url = gBrowser.currentURI;
+    var localUrl = url.clone();
+    var host = localUrl.host;
+    var msg = document.getElementById("pcookieStrings").getString("alert.warning");
+    if(host == "") {
+      alert(msg);
+      return;
+    }
+
+    var sdom = host;
+    try {
+      if (prefUtils.getPref("bool", "pcookies.stripwww"))
+        sdom = pCookie.stripWWW(host);
+    } catch(e) {
+      prefUtils.setPref("bool", "pcookies.stripwww", false)
+    }
+    localUrl.host = sdom;
+    host = sdom;
+    openDialog("chrome://pcookie/content/pcDialog.xul", "",
+      "centerscreen,chrome,dialog,resizable=no,dependent",
+      host, localUrl, window);
+  },
+
+  stripWWW: function(url)
+  {
+    var reg = /www./i;
+    return url.replace(reg, "");
+  },
+
+
+  getPerm: function()
+  {
+    if(pCookie.permMan == null)
+      pCookie.permMan = Components.classes["@mozilla.org/permissionmanager;1"].getService()
+                          .QueryInterface(Components.interfaces.nsIPermissionManager);
+    return pCookie.permMan;
+
+  },
+
+
+  initDialog: function()
+  {
+    var tabsbox = document.getElementById("tabs");
+    if(window.arguments[2].gBrowser.browsers.length <= 1)
+      tabsbox.setAttribute("hidden", true);
+
+    var inurl = window.arguments[0];
+    pCookie.urlbox = document.getElementById("url");
+    pCookie.urlbox.value = inurl;
+    pCookie.updateStatus();
+  },
+
+
+  onTabsChanged: function()
+  {
+    var tabsbox = document.getElementById("tabs");
+    var urlfield = document.getElementById("url");
+    if(tabsbox.checked)
+      urlfield.disabled = true;
+    else
+      urlfield.disabled = false;
+  },
+
+  updateStatus: function()
+  {
+    var label = document.getElementById("stateValue");
+    var perm = pCookie.checkState();
+    label.value = perm;
+
+    var radio = document.getElementById("pRadiogroup");
+    switch (perm) {
+      case "allowed" : radio.selectedIndex = 0;
+        break;
+      case "session" : radio.selectedIndex = 1;
+        break;
+      case "blocked" : radio.selectedIndex = 2;
+        break;
+      case "not set" : radio.selectedIndex = 0;
+        break;
+    }
+  },
+
+  onDialogOK: function()
+  {
+    var url = window.arguments[1];
+    var permMan = pCookie.getPerm();
+    if(permMan == null) return;
+    var rgroup = document.getElementById("pRadiogroup");
+    var sel = rgroup.selectedItem.id;
+    var action; 
+    if(sel == "allow")
+      action = permMan.ALLOW_ACTION;
+    else if (sel == "session")
+      action = 8;
+    else if (sel == "block")
+      action = permMan.DENY_ACTION;
+
+    if(document.getElementById("tabs").checked) { // set for all tabs
+      pCookie.setForTabs(action, window.arguments[2]);
+      return;
+    }
+    var outurl = pCookie.urlbox.value;
+    if(outurl != "") {
+      url.host = outurl;
+      permMan.remove(outurl, "cookie");
+      if(sel != "remove")
+        permMan.add(url, "cookie", action);
+    }
+    window.arguments[2].pCookie.updateButton(url);
+  },
+
+
+  setForTabs: function(aAction, aWindow) {
+    var permMan = pCookie.getPerm();
+    if(permMan == null) return;
+    var browsers = aWindow.gBrowser.browsers;
+
+    for (var i=0; i<browsers.length; ++i) {
+      var webNav = browsers[i].webNavigation;
+      var host = webNav.currentURI.host;
+    if (prefUtils.getPref("bool", "pcookies.stripwww"))
+        host = pCookie.stripWWW(host);
+      if(host == "" || host == " ")
+        continue;
+      permMan.remove(host, "cookie");
+      if(aAction == permMan.ALLOW_ACTION || aAction == permMan.DENY_ACTION ||
+            aAction == 8) {
+        var url = webNav.currentURI;
+        url = url.clone();
+        url.host = host;
+        permMan.add(url, "cookie", aAction);
+      }
+    }
+    aWindow.pCookie.updateButton();
+  },
+
+
+  checkState: function()
+  {
+    // not set:0  allowed:1  blocked:2 session:8
+    var strings = document.getElementById("pcookieStrings");
+    var url = pCookie.urlbox.value;
+    var permMan = pCookie.getPerm();
+    var retval = "error";
+    if(permMan == null) return retval;
+    var uri = Components.classes["@mozilla.org/network/standard-url;1"]
+        .createInstance(Components.interfaces.nsIURI);
+    uri.spec = url;
+    var state = permMan.testPermission(uri, "cookie");
+    switch (state) {
+      case 0: retval = strings.getString("status.notset");
+        break;
+      case 1: retval = strings.getString("status.allowed");
+        break;
+      case 2: retval = strings.getString("status.blocked");
+        break;
+      case 8: retval = strings.getString("status.session");
+        break;
+    }
+    return retval;
+  },
+
+
+  updateButton: function(aURI)
+  {
+    try {
+      var permMan = pCookie.getPerm();
+      if(permMan == null || aURI == null) return;
+      var state = permMan.testPermission(aURI, "cookie");
+      var button = document.getElementById("pcookie-status-button");
+      if(!button)
+        return;
+        
+      switch (state) {
+        case 0: button.setAttribute("status", "notset");
+          break;
+        case 1: button.setAttribute("status", "allowed");
+          break;
+        case 2: button.setAttribute("status", "blocked");
+          break;
+        case 8: button.setAttribute("status", "session");
+          break;
+      }
+    }catch(e){if(_DEBUG) alert(e);}
+  }
+
+};// end pCookie
+
+addEventListener("load", pCookie.init, false);
+addEventListener("unload", pCookie.exit, false);
+
---
+++ src/chrome/pcookie/content/pcookie/prefUtils.js
@@ -0,0 +1,198 @@
+/**
+ * prefUtils.js
+ * 0.4
+ * 2004-12-13
+ * Daniel Lindkvist
+ **/
+
+// settings
+_DEBUG = false;
+AUTO_SET_PREFS = true;
+
+if(_DEBUG) dump("in prefutils\n");
+
+
+var prefUtils =
+{
+  onLoad: function(aDialogId)
+  {
+    var dialog = document.getElementById(aDialogId);
+    if(_DEBUG) dump("in onLoad\n");
+    if(!dialog) {
+      if(_DEBUG) dump("No dialog id supplied?\n");
+      return;
+    }
+    var prefElements = dialog.getElementsByAttribute("prefstring", "*");
+    if(!prefElements) {
+      if(_DEBUG) dump("prefElements empty\n");
+      return;
+    }
+    for(var i=0; i<prefElements.length; i++) {
+      var prefstring = prefElements[i].getAttribute("prefstring");
+      var prefid = prefElements[i].getAttribute("id");
+      var preftype = prefElements[i].getAttribute("preftype");
+      var elt = prefElements[i].localName;
+      var prefdefval = prefElements[i].getAttribute("prefdefval");
+      if(!prefdefval)
+        prefdefval = "";
+
+      if (!preftype) {
+        if (elt == "textbox")
+          preftype = "string";
+        else if (elt == "checkbox" || elt == "button")
+          preftype = "bool";
+        else if (elt == "radiogroup" || elt == "menulist")
+          preftype = "int";
+      }
+      var prefvalue = prefUtils.getPref(preftype, prefstring, prefdefval);
+      if(_DEBUG) dump("pref collected: "+ prefvalue+"\n");
+      if(_DEBUG) dump("elementtype: "+ elt+"\n\n");
+      if(prefvalue == "!/!ERROR_UNDEFINED_PREF!/!") {
+        prefvalue = prefdefval;
+        if(AUTO_SET_PREFS)
+          prefUtils.setPref(preftype, prefstring, prefvalue);
+      }
+      switch(elt) {
+        case "textbox":
+          prefElements[i].value = prefvalue;
+          break;
+        case "checkbox":
+          prefElements[i].checked = prefvalue;
+          break;
+        case "button":
+          prefElements[i].checked = prefvalue;
+          break;
+        case "radiogroup":
+          prefElements[i].value = prefvalue;
+          break;
+        case "menulist":
+          prefElements[i].selectedIndex = prefvalue;
+          break;
+      }
+    }
+    if(_DEBUG) dump("init finished\n");
+  },// end init
+
+
+  onOK: function(aDialogId, aObserverTopicId)
+  {
+    var dialog = document.getElementById(aDialogId);
+    var prefElements = dialog.getElementsByAttribute("prefstring", "*");
+
+    for(var i=0; i<prefElements.length; i++) {
+      var prefstring = prefElements[i].getAttribute("prefstring");
+      var preftype = prefElements[i].getAttribute("preftype");
+
+      var elt = prefElements[i].localName;
+      if (!preftype) {
+        if (elt == "textbox")
+          preftype = "string";
+        else if (elt == "checkbox" || elt == "button")
+          preftype = "bool";
+        else if (elt == "radiogroup" || elt == "menulist")
+          preftype = "int";
+      }
+      var prefvalue; 
+      if (elt == "checkbox")
+        prefvalue = prefElements[i].checked;
+      else if (elt == "button")
+        prefvalue = prefElements[i].checked;
+      else if (elt == "radiogroup" || elt == "menulist")
+        prefvalue = prefElements[i].selectedIndex;
+      else if (elt == "textbox")
+        prefvalue = prefElements[i].value;
+
+      prefUtils.setPref(preftype, prefstring, prefvalue);
+      if(_DEBUG) dump("setpref:" +prefstring+" "+ prefvalue + "\n");
+    }
+    // notify observers
+    if (aObserverTopicId)
+    Components.classes["@mozilla.org/observer-service;1"]
+        .getService(Components.interfaces.nsIObserverService)
+        .notifyObservers(null, aObserverTopicId, "");
+    return true;
+  },// end onOk
+
+
+  getPref: function(aPrefType, aPrefString)
+  {
+    if(_DEBUG) dump("in getpref\n");
+    var pref = Components.classes["@mozilla.org/preferences-service;1"]
+        .getService(Components.interfaces.nsIPrefBranch);
+    try {
+      switch(aPrefType) {
+        case "bool":
+          return pref.getBoolPref(aPrefString);
+        case "int":
+          return pref.getIntPref(aPrefString);
+        case "localizedstring":
+          return pref.getLocalizedUnicharPref(aPrefString); // not working?
+        case "color":
+        case "string":
+        default:
+          return pref.getCharPref(aPrefString); 
+      }
+    }catch(e) {
+      if(_DEBUG) {
+        dump("*** no default pref for " + aPrefType + " pref: " + aPrefString + "\n");
+        dump(e + "\n");
+      }
+    }
+    return "!/!ERROR_UNDEFINED_PREF!/!";
+  },
+
+
+  setPref: function(aPrefType, aPrefString, aValue)
+  {
+    var pref = Components.classes["@mozilla.org/preferences-service;1"]
+        .getService(Components.interfaces.nsIPrefBranch);
+    try {
+      switch (aPrefType) {
+        case "bool":
+          pref.setBoolPref(aPrefString, aValue);
+          break;
+        case "int":
+          pref.setIntPref(aPrefString, aValue);
+          break;
+        case "color":
+        case "string":
+        case "localizedstring":
+        default:
+          pref.setCharPref(aPrefString, aValue);
+          break;
+      }
+    }catch(e){
+      if(_DEBUG) dump(e + "\n");
+    }
+  },
+
+  pickPath: function(aId)
+  {
+    var fp = Components.classes["@mozilla.org/filepicker;1"]
+             .createInstance(Components.interfaces.nsIFilePicker);
+    fp.init(window, "", fp.modeGetFolder);
+    if (fp.show() == fp.returnCancel)
+      return;
+    var path = fp.file.target;
+    var element = document.getElementById(aId);
+    if(element)
+      element.value = path;
+    return path;
+  },
+
+
+  initControls:function(ids)
+  {
+    if(ids.length < 2)
+      return;
+    var master = document.getElementById(ids[0]);
+    var slave;
+    for(var x=1; x<ids.length; x++) {
+      slave = document.getElementById(ids[x]);
+      if(master.checked)
+        slave.disabled = true;
+      else
+        slave.disabled = false;
+    }
+  }
+};// end prefUtils
