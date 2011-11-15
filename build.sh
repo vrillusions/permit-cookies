fe9a1be3d5b1a535ea388cbb649144667aa0d574
Magrath <root@magrath.eu>
2011-11-15 22:56:05 +0000

    improve xpi structure and fix build

--- build.sh
+++ build.sh
@@ -7,14 +7,8 @@ cp -r src build
 # Delete the DS_Store files
 find . -name *.DS_Store -type f -exec rm {} \;
 
-# Compress pcookie.jar file
-cd build/chrome && \
-zip -r pcookie.jar pcookie/* && \
-rm -rf pcookie
-
-#Compress whole project
-cd ../../build && \
-rm -rf chrome/.DS_Store && \
+# Compress src into xpi
+cd build && \
 zip -r pcookie.xpi -xi * && \
 rm -rf chrome && \
 rm -rf chrome.manifest && \
